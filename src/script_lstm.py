661736b5fdce311859c582de2e774fabcf8240c2
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2025-05-30 10:27:30 +0200

    update

--- src/script_lstm.py
+++ src/script_lstm.py
@@ -1,62 +1,153 @@
-DATAPATH = 'data/Sunspots.csv'
+"""
+    Script used for prediction of sunspot numbers using LSTM.
+    The script loads the dataset, preprocesses it, and visualizes the data.
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+    Author: Bogdan Ljubinković
+    Date: May 2025
+"""
+
+DATA_PATH = 'data/SN_m_tot_V2.0.csv'
 
 import pandas as pd
 import numpy as np
 
-import tensorflow as tf
-
 import warnings
 warnings.filterwarnings("ignore")
 
-data = pd.read_csv(DATAPATH)
+# loading dataset
+sunspots = pd.read_csv(DATA_PATH, sep=';')
 
-# print(data.info()) # int64, object, float64
+columns = ["year", "month", "decimal_year", "number", "deviation", "observations", "flag"]
 
-sunspots = data.iloc[:, -1] # the last column is the sunspots data
-dates = data.iloc[:, -2] # the second column is the dates
-# print(sunspots.values)
-
-"""
-
-plt.figure(figsize=(20, 6))
-plt.plot(dates, sunspots)
-plt.plot(sunspots[:72])    # from 1749 to 1755 (which is the new solar cycle) 
-plt.plot(sunspots[72:72+132])  # first cycle
-plt.plot(sunspots[-13:])  # current cycle
-plt.ylabel(data.columns[-1])  
-plt.xlabel(data.columns[-2])
-plt.title('Sunspot numbers over time')
-plt.xticks(ticks=np.arange(0, len(dates), 150), labels=dates.iloc[::150], rotation=45)
-plt.legend(['All data', '1749-1755', 'First cycle', 'Current cycle'])
-plt.tight_layout()
-plt.show()
+print(sunspots.info())
 
 """
-
+ #   Column        Non-Null Count  Dtype  
+---  ------        --------------  -----  
+ 0   year          3316 non-null   int64  
+ 1   month         3316 non-null   int64  
+ 2   decimal_year  3316 non-null   float64
+ 3   number        3316 non-null   float64
+ 4   deviation     3316 non-null   float64
+ 5   observations  3316 non-null   int64  
+ 6   flag          3316 non-null   int64  
 """
 
-plt.figure(figsize=(20, 6))
-plt.plot(range(72, len(sunspots)), sunspots[72:], label='Sunspots')
-plt.title('Sunspot numbers from 1755 - 2019')
-plt.xlim(72, len(sunspots) - 12)
+# eliminate unnecessary columns
+sunspots.drop(columns=['flag', 'deviation', 'observations'], inplace=True)
 
-# calculate year labels for every 11th year starting from 1755
-start_year = 1755
-num_years = (len(sunspots) - 72) // 12
-years = [start_year + 11 * i for i in range((num_years // 11) + 1)]
-xticks = [72 + 12 * 11 * i for i in range(len(years))]
-
-plt.xticks(xticks, years, rotation=45)
-plt.xlabel('Year')
-plt.ylabel('Sunspot Number')
-plt.legend()
-plt.tight_layout()
-plt.show()
+print(sunspots.info())
 
 """
+Data columns (total 4 columns):
+ #   Column        Non-Null Count  Dtype  
+---  ------        --------------  -----  
+ 0   year          3316 non-null   int64  
+ 1   month         3316 non-null   int64  
+ 2   decimal_year  3316 non-null   float64
+ 3   number        3316 non-null   float64
+dtypes: float64(2), int64(2)
+memory usage: 103.8 KB
+None
+"""
 
-# RNN has problem with long sequences, so we will use LSTM
+# merge year and month into a single date column
+sunspots['date'] = pd.to_datetime(sunspots[['year', 'month']].assign(day=1))
+
+# eliminate year, month, and decimal_year columns
+sunspots.drop(columns=['year', 'month', 'decimal_year'], inplace=True)
+print(sunspots.info())
+
+"""
+ #   Column  Non-Null Count  Dtype         
+---  ------  --------------  -----         
+ 0   number  3316 non-null   float64       
+ 1   date    3316 non-null   datetime64[ns]
+dtypes: datetime64[ns](1), float64(1)
+memory usage: 51.9 KB
+"""
+
+# plotting the sunspot numbers over time
+
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as plt
+
+# plot graph, where first 77 data points are in red (first solar cycle)
+# next 134 data points are in green (second solar cycle)
+# and the last 50 data points are in orange (solar cycle 25)
+
+def first_plot():
+    plt.figure(figsize=(18, 6))
+    plt.plot(sunspots['date'], sunspots['number'], label='Sunspot Number', color='blue')
+    plt.plot(sunspots['date'][:77], sunspots['number'][:77], label='First Solar Cycle', color='red')
+    plt.plot(sunspots['date'][77:210], sunspots['number'][77:210], label='Second Solar Cycle', color='green')
+    plt.plot(sunspots['date'][-60:], sunspots['number'][-60:], label='Solar Cycle 25', color='orange')
+    plt.xlabel('Date')
+    plt.ylabel('Sunspot Number')
+    plt.title('Sunspot Number Over Time with Solar Cycles Highlighted')
+    plt.xticks(rotation=45)
+    plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(12))  # maximum 12 ticks on x-axis
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))  # format x-axis as Year-Month
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
+
+# first_plot()
+
+# remove first 77 and last 60 data points (incomplete solar cycles)
+sunspots = sunspots.iloc[77:-60].reset_index(drop=True)
+
+# plot graph, where x axis shows first date and every 132th date after that till the end
+new_dates = []
+for i in range(0, len(sunspots), 132):
+    new_dates.append(sunspots['date'].iloc[i])
+
+def second_plot(new_dates):
+    plt.figure(figsize=(18, 6))
+    plt.plot(sunspots['date'], sunspots['number'], label='Sunspot Number', color='blue')
+    plt.xticks(new_dates, rotation=45)
+    plt.xlabel('Date')
+    plt.ylabel('Sunspot Number')
+    plt.title('Sunspot Number Over Time with Selected Dates')
+    
+    # new_dates on x-axis
+
+    new_dates = mdates.date2num(new_dates)  # convert to matplotlib date format
+
+    plt.gca().xaxis.set_major_locator(ticker.FixedLocator(new_dates))
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))  # format x-axis as Year-Month
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
+
+# second_plot(new_dates)
+
+# plotting the acf
+from statsmodels.graphics.tsaplots import plot_acf
+def plot_acf_graph():
+    fig = plt.figure(figsize=(16, 6))
+    plot_acf(sunspots['number'], lags=30, alpha=0.05, title='Autocorrelation Function of Sunspot Numbers', ax=plt.gca())
+    plt.tight_layout()
+    plt.show()
+
+# plot_acf_graph()
+"""
+    As we can see from the ACF graph after k=29 there is no autocorrelation
+    Which means this is a time series
+"""
+
+# 
+
+
+
+
+
+
+
+
+
+
 
